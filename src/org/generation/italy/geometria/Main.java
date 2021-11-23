310fd7311b9e370a32aa170cdc9cfb26dadf80ce
Mattia Maio <mattia.maio10@gmail.com>
2021-11-23 15:57:47 +0100

    apportate correzioni

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -7,12 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		Rettangolo rettangolo = new Rettangolo();
-
 		System.out.print("Base: ");
-		rettangolo.base = scan.nextInt();
+		int base = scan.nextInt();
+
 		System.out.print("\nAltezza: ");
-		rettangolo.altezza = scan.nextInt();
+		int altezza = scan.nextInt();
+
+		Rettangolo rettangolo = new Rettangolo(base, altezza);
 
 		System.out.println("\nPerimetro: " + rettangolo.calcolaPerimetro());
 
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -1,37 +1,25 @@
 package org.generation.italy.geometria;
 
 public class Rettangolo {
-	//attributi
+	// attributi
 	int base;
 	int altezza;
-	int perimetro;
-	int area;
-	
-	//costruttore
-	Rettangolo(){
-		this.base = 0;
-		this.altezza = 0;
-	}	
-//	Rettangolo(int base, int altezza){
-//		this.base = base;
-//		this.altezza = altezza;
-//	}
-	
-	//metodi
+
+	// costruttore
+	Rettangolo(int base, int altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+
+	// metodi
 	int calcolaPerimetro() {
-		perimetro = (base + altezza) * 2;
+		int perimetro = (base + altezza) * 2;
 		return perimetro;
 	}
-	
+
 	int calcolaArea() {
-		area = base * altezza;
+		int area = base * altezza;
 		return area;
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
